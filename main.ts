42a874f34be4782af37fc1037a2d78dfd13d9e45
Andrew Fichman <andrew.fichman@gmail.com>
2019-09-10 10:37:10 -0400

    adds .json upload feature

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 import * as vega from "vega";
 import VegaTransformPostgres from "vega-transform-pg";
+const querystring = require('querystring');
+const http = require('http');
+
+const postgresConnectionString = 'postgres://localhost:5432/voyager';
 
 function run(spec:vega.Spec) {
   VegaTransformPostgres.setPostgresConnectionString('postgres://localhost:5432/voyager');
@@ -9,7 +13,7 @@ function run(spec:vega.Spec) {
     method: 'POST',
     path: '/query',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
   });
   (vega as any).transforms["postgres"] = VegaTransformPostgres;
@@ -33,4 +37,48 @@ function handleVegaSpec() {
   (<HTMLInputElement>document.getElementById("vega-spec")).value = "";
 }
 
+function handleData() {
+  const reader = new FileReader();
+  let filename;
+  reader.onload = function(e:any) {
+    if(filename.slice(filename.length-'.json'.length) != '.json') {
+      throw `file ${filename} must have .json extension`;
+    }
+    const tableName = filename.slice(0,(filename.length-'.json.'.length)+1);
+    const data = JSON.parse(e.target.result);
+    const postData = querystring.stringify({
+      name: tableName,
+      data: JSON.stringify(data),
+      postgresConnectionString: postgresConnectionString
+    });
+    const httpOptions = {
+      hostname: 'localhost',
+      port: 3000,
+      method: 'POST',
+      path: '/createSql',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      'Content-Length': Buffer.byteLength(postData)
+    };
+    const req = http.request(httpOptions, res => {
+      let result = '';
+      res.on('data', chunk => {
+        result += chunk;
+      });
+      res.on('end', () => {
+        if(res.statusCode === 400) {
+          throw `${res.statusMessage}: ${result}`;
+        }
+      });
+    });
+    req.write(postData);
+    req.end();
+  }
+  filename = this.files[0].name;
+  reader.readAsText(this.files[0]);
+  (<HTMLInputElement>document.getElementById("data")).value = "";
+}
+
 document.getElementById("vega-spec").addEventListener("change", handleVegaSpec, false);
+document.getElementById("data").addEventListener("change", handleData, false);
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg');
+const format = require('pg-format');
 
 // Postgres connection pools.
 const pools = {};
@@ -41,6 +42,138 @@ app.post('/query', async (req: any, res: any) => {
     client = await pool.connect();
     const results = await client.query(req.body.query);
     res.status(200).send(results);
+  } catch(err) {
+    handleError(err, res);
+  } finally {
+    if(client) {
+      client.release();
+    }
+  }
+});
+
+function postgresTypeFor(value : any): string {
+  // FixMe: want to use INTs too, if possible. Client needs to send more data.
+  const type = typeof value;
+  if(type === 'string') {
+    return 'VARCHAR(256)';
+  } else if(type === 'number') {
+    return 'FLOAT';
+  } else if(type === 'boolean') {
+    return 'BOOLEAN';
+  } else {
+    console.log('ERROR: undefined type: \'' + type + '\'');
+  }
+}
+
+function postgresSchemaFor(dataObj: any): string {
+  const schema: any = {};
+  for(var property in dataObj) {
+    if(dataObj.hasOwnProperty(property)) {
+      const pgType = postgresTypeFor(dataObj[property]);
+      schema[property] = postgresTypeFor(dataObj[property]);
+    }
+  }
+  return schema;
+}
+
+function createTableQueryStrFor(tableName: string, schema: any): string {
+  let out: string = 'CREATE TABLE ' + tableName + '('
+  let first: boolean = true;
+  for(var attrName in schema) {
+    if(!schema.hasOwnProperty(attrName)) {
+      continue;
+    }
+    let attrType: string = schema[attrName];
+    if(first) {
+      first = false;
+    } else {
+      out += ', ';
+    }
+    out += (attrName + ' ' + attrType)
+  }
+  out += ');';
+  return out;
+}
+
+function listToSQLTuple(l: any[], keepQuotes: boolean): string {
+  let out: string = JSON.stringify(l);
+  out = out.substring(1, out.length - 1);
+  out = out.replace(/'/g, '\'\'');
+  out = out.replace(/"/g, keepQuotes? '\'' : '');
+  return out;
+}
+
+app.post('/createSql', async (req: any, res: any) => {
+  let client: any;
+  try {
+    if(!req.body.postgresConnectionString) {
+      throw 'request body must define postgresConnectionString property';
+    }
+    if(!req.body.data) {
+      throw 'request body must define data property';
+    }
+    if(!req.body.name) {
+      throw 'request body must define name property';
+    }
+
+    // Connect to postgres
+    const pool = poolFor(req.body.postgresConnectionString);
+    client = await pool.connect();
+
+    // Check if table exists yet
+    let exists = false;
+    const existsQueryStr = 'SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name='+
+      '\'' + req.body.name.toLowerCase() + '\');'
+    const response = await client.query(existsQueryStr);
+    if(response.rows[0]['exists']) {
+      exists = true;
+      console.log('INFO: table ' + req.body.name + ' already exists.');
+    } else {
+      exists = false;
+      console.log('INFO: table ' + req.body.name + ' does not exist');
+    }
+
+    const data = JSON.parse(req.body.data);
+    const schema: any = postgresSchemaFor(data[0]);
+
+    // Create table if it doesn't exist yet
+    if(!exists) {
+      console.log('INFO: creating table ' + req.body.name);
+      console.log('INFO: built postgres schema: '+JSON.stringify(schema));
+      const createTableQueryStr = createTableQueryStrFor(req.body.name, schema);
+      console.log('INFO: running create query: ' + createTableQueryStr);  
+      await client.query(createTableQueryStr);
+    }
+
+    // Insert values
+  
+    // Build attribute list string e.g. (attr1, attr2, attr3)
+    let attrNames: string[] = [];
+    for(const attrName in schema) {
+      if(!schema.hasOwnProperty(attrName)) {
+        continue;
+      }
+      attrNames.push(attrName);
+    }
+    const attrNamesStr = listToSQLTuple(attrNames, false);
+  
+    // Transform data from JSON format into a 2d array where each row is a list of attribute values
+    // with the same attribute order as the attribute list string above.
+    const rows: any[] = [];
+    for(let i: number = 0; i < data.length; i++) {
+      const item: any = data[i];
+      const row: any[] = [];
+      for(let j: number = 0; j < attrNames.length; j++) {
+        row.push(item[attrNames[j]]);
+      }
+      rows.push(row);
+    }
+
+    // Execute the insert queries.
+    const queryStr = format('INSERT INTO ' + req.body.name + ' (' + attrNamesStr + ') VALUES %L', rows);
+    console.log('INFO: running insert queries for ' + req.body.name);
+    await client.query(queryStr);
+    res.status(200).send();
   } catch(err) {
     handleError(err, res);
   } finally {
