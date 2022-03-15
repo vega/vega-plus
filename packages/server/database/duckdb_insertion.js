35023d7865cc563683838e0df34743a9521196dc
JunranY <junran@cs.washington.edu>
2022-03-15 12:49:52 -0700

    upgrade duckdb and sample-loading script

--- packages/server/database/duckdb_insertion.js
+++ packages/server/database/duckdb_insertion.js
@@ -1,6 +1,6 @@
 var duckdb = require('duckdb');
 var db = new duckdb.Database('./packages/server/database/scalable-vega.db');
-var format = require('pg-format');
+const format = require('pg-format');
 global.fetch = require("node-fetch");
 db.all('SELECT 42 AS fortytwo', function (err, res) {
     if (err) {
@@ -11,7 +11,7 @@ db.all('SELECT 42 AS fortytwo', function (err, res) {
 function postgresTypeFor(value) {
     // FixMe: want to use INTs too, if possible.
     // Client needs to send more type data in this case.
-    var type = typeof value;
+    const type = typeof value;
     if (type === 'string') {
         return 'VARCHAR(256)';
     }
@@ -26,7 +26,7 @@ function postgresTypeFor(value) {
     }
 }
 function postgresSchemaFor(dataObj) {
-    var schema = {};
+    const schema = {};
     for (var property in dataObj) {
         if (dataObj.hasOwnProperty(property)) {
             schema[property] = postgresTypeFor(dataObj[property]);
@@ -35,13 +35,13 @@ function postgresSchemaFor(dataObj) {
     return schema;
 }
 function createTableQueryStrFor(tableName, schema) {
-    var out = 'create table ' + tableName + '(';
-    var first = true;
+    let out = 'create table ' + tableName + '(';
+    let first = true;
     for (var attrName in schema) {
         if (!schema.hasOwnProperty(attrName)) {
             continue;
         }
-        var attrType = schema[attrName];
+        let attrType = schema[attrName];
         if (first) {
             first = false;
         }
@@ -54,7 +54,7 @@ function createTableQueryStrFor(tableName, schema) {
     return out;
 }
 function listToSQLTuple(l, keepQuotes) {
-    var out = JSON.stringify(l);
+    let out = JSON.stringify(l);
     out = out.substring(1, out.length - 1);
     out = out.replace(/'/g, '\'\'');
     out = out.replace(/"/g, keepQuotes ? '\'' : '');
@@ -65,37 +65,38 @@ var i;
 for (i = 0; i < test_cases.length; i++) {
     var data_name = test_cases[i];
     var client = db.connect();
-    var data = require('../../../sample_data/data/' + data_name + '.json');
-    var schema = postgresSchemaFor(data[0]);
+    const data = require('../../../sample_data/data/' + data_name + '.json');
+    const schema = postgresSchemaFor(data[0]);
     console.log('creating table ' + data_name);
     console.log('built postgres schema: ' + JSON.stringify(schema));
-    var createTableQueryStr = createTableQueryStrFor(data_name, schema);
+    const createTableQueryStr = createTableQueryStrFor(data_name, schema);
     console.log('running create query: ' + createTableQueryStr);
     client.run(createTableQueryStr);
     // Insert values
     // Build attribute list string e.g. (attr1, attr2, attr3)
-    var attrNames = [];
-    for (var attrName in schema) {
+    let attrNames = [];
+    for (const attrName in schema) {
         if (!schema.hasOwnProperty(attrName)) {
             continue;
         }
         attrNames.push(attrName);
     }
-    var attrNamesStr = listToSQLTuple(attrNames, false);
+    const attrNamesStr = listToSQLTuple(attrNames, false);
     // Transform data from JSON format into a 2d array where each row is a list of attribute values
     // with the same attribute order as the attribute list string above.
-    var rows = [];
-    for (var i_1 = 0; i_1 < data.length; i_1++) {
-        var item = data[i_1];
-        var row = [];
-        for (var j = 0; j < attrNames.length; j++) {
+    const rows = [];
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const row = [];
+        for (let j = 0; j < attrNames.length; j++) {
             row.push(item[attrNames[j]]);
         }
         rows.push(row);
     }
     // Execute the insert queries.
-    var queryStr = format('insert into ' + data_name + ' (' + attrNamesStr + ') values %L', rows);
+    const queryStr = format('insert into ' + data_name + ' (' + attrNamesStr + ') values %L', rows);
     console.log('running insert queries for ' + data_name);
     client.run(queryStr);
     console.log('insert queries complete');
 }
+//# sourceMappingURL=duckdb_insertion.js.map
